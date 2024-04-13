09ade43d8baa9aba8ae71acd002d6e0875caa0b9
nuwan-dharmarathna <dharmarathna1220@gmail.com>
2024-04-13 09:24:20 +0530

    add logger file

--- app.py
+++ app.py
@@ -2,12 +2,18 @@ from flask import Flask, render_template, request, redirect
 
 from prediction_pipeline import get_prediction
 
+from logger import logging
+
 data  = '--Your Comment Status--'
 
 app = Flask(__name__)
 
+logging.info('Flask server Started')
+
 @app.route('/')
 def index():
+    logging.info('========== Open Home Page ==========')
+    
     return render_template('index.html', data = data)
 
 @app.route('/', methods=['post'])
